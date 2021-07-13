5da80cc08d5ee09c18d35ba55a9e8c4338b6661f
talentpierre <talentpierre@gmail.com>
2021-07-13 23:24:43 +0200

    connect ssh

--- Bitcoin_Dashboard/Page2.Designer.cs
+++ Bitcoin_Dashboard/Page2.Designer.cs
@@ -28,19 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSSH = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSSH
+            // 
+            this.btnSSH.Location = new System.Drawing.Point(480, 42);
+            this.btnSSH.Name = "btnSSH";
+            this.btnSSH.Size = new System.Drawing.Size(75, 23);
+            this.btnSSH.TabIndex = 0;
+            this.btnSSH.Text = "Connect";
+            this.btnSSH.UseVisualStyleBackColor = true;
+            this.btnSSH.Click += new System.EventHandler(this.btnSSH_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(224, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(227, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
             // 
             // Page2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSSH);
             this.Name = "Page2";
             this.Size = new System.Drawing.Size(788, 506);
+            this.Load += new System.EventHandler(this.Page2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSSH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
